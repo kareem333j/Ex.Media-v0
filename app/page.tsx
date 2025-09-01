58b9bae9adce5413cb7953067750fc8310ba8897
karim magdy <kareem147j@gmail.com>
2025-09-01 15:22:34 +0300

    fix an error in main page

--- app/page.tsx
+++ app/page.tsx
@@ -961,11 +961,11 @@ export default function ExMediaLanding() {
                   : "Transforming digital experiences through innovative media solutions."}
               </p>
             </div>
-            <div>
+            <div className="w-[100%] overflow-hidden">
               <h4 className={`font-semibold mb-4 text-primary ${language === "ar" ? "font-arabic" : ""}`}>
                 {language === "ar" ? "معلومات التواصل" : "Contact Info"}
               </h4>
-              <div dir="ltr" className="space-y-2 flex flex-col">
+              <div dir="ltr" className={`space-y-2 overflow-hidden flex flex-col justify-center items-center ${language === "ar" ? "md:items-end" : "md:items-start"} m-0`}>
                 <div
                   dir={language === "ar" ? "rtl" : "ltr"}
                   className={`flex items-center space-x-2`}
@@ -987,13 +987,12 @@ export default function ExMediaLanding() {
                   <svg className="w-4 h-4 text-primary" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
                   </svg>
-                  <a
+                  <p
                     dir="ltr"
-                    href="mailto:Expointmedia@gmail.com"
                     className={`text-sm hover:text-primary transition-colors duration-300 cursor-pointer hover:underline ${language === "ar" ? "font-arabic" : ""}`}
                   >
                     +20 1000028787
-                  </a>
+                  </p>
                 </div>
               </div>
             </div>
@@ -1001,7 +1000,7 @@ export default function ExMediaLanding() {
               <h4 className={`font-semibold mb-4 text-primary ${language === "ar" ? "font-arabic" : ""}`}>
                 {language === "ar" ? "تابعنا" : "Follow Us"}
               </h4>
-              <div dir={language === "ar" ? "rtl" : "ltr"} className="flex w-100 space-x-4">
+              <div dir={language === "ar" ? "rtl" : "ltr"} className={`flex w-100 space-x-4 items-center justify-center ${language === "ar" ? "md:justify-start" : "md:justify-start"} `}>
                 <a href="https://www.instagram.com/expointmedia?igsh=eHo5YThiM215aWtqcQ==" target="_blank" className="text-primary inline-flex hover:text-accent transition-colors duration-300">
                   <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                     <path d="M320.3 205C256.8 204.8 205.2 256.2 205 319.7C204.8 383.2 256.2 434.8 319.7 435C383.2 435.2 434.8 383.8 435 320.3C435.2 256.8 383.8 205.2 320.3 205zM319.7 245.4C360.9 245.2 394.4 278.5 394.6 319.7C394.8 360.9 361.5 394.4 320.3 394.6C279.1 394.8 245.6 361.5 245.4 320.3C245.2 279.1 278.5 245.6 319.7 245.4zM413.1 200.3C413.1 185.5 425.1 173.5 439.9 173.5C454.7 173.5 466.7 185.5 466.7 200.3C466.7 215.1 454.7 227.1 439.9 227.1C425.1 227.1 413.1 215.1 413.1 200.3zM542.8 227.5C541.1 191.6 532.9 159.8 506.6 133.6C480.4 107.4 448.6 99.2 412.7 97.4C375.7 95.3 264.8 95.3 227.8 97.4C192 99.1 160.2 107.3 133.9 133.5C107.6 159.7 99.5 191.5 97.7 227.4C95.6 264.4 95.6 375.3 97.7 412.3C99.4 448.2 107.6 480 133.9 506.2C160.2 532.4 191.9 540.6 227.8 542.4C264.8 544.5 375.7 544.5 412.7 542.4C448.6 540.7 480.4 532.5 506.6 506.2C532.8 480 541 448.2 542.8 412.3C544.9 375.3 544.9 264.5 542.8 227.5zM495 452C487.2 471.6 472.1 486.7 452.4 494.6C422.9 506.3 352.9 503.6 320.3 503.6C287.7 503.6 217.6 506.2 188.2 494.6C168.6 486.8 153.5 471.7 145.6 452C133.9 422.5 136.6 352.5 136.6 319.9C136.6 287.3 134 217.2 145.6 187.8C153.4 168.2 168.5 153.1 188.2 145.2C217.7 133.5 287.7 136.2 320.3 136.2C352.9 136.2 423 133.6 452.4 145.2C472 153 487.1 168.1 495 187.8C506.7 217.3 504 287.3 504 319.9C504 352.5 506.7 422.6 495 452z" />
